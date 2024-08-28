b730f9c99856a5c05bbbc4c42525e7d9943d3ac0
EnzoSottoRib <enzosr0309@gmail.com>
2024-08-28 09:28:47 -0300

    primeiro commit

---
+++ Aprendendo-C-Sharp/C#/PrimeiroProjeto/Ex03/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Receber dois números inteiros e informar qual valor lido é o menor e qual é o maior. */
+
+Console.WriteLine("Digite o primeiro número: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num1 > num2){
+    Console.WriteLine("O primeiro é maior!");
+} else if (num1 == num2){
+    Console.WriteLine("Os dois são iguais!");
+} else {
+    Console.WriteLine("O segundo é maior!");
+}
---
+++ Aprendendo-C-Sharp/C#/PrimeiroProjeto/Ex06a/Program.cs
@@ -0,0 +1,43 @@
+﻿
+//1- criar um vetor para receber os valores inteiros
+//2- colocar os valores no vetor aleatoriamente
+//3- ordenar os valores bubble sort
+//4- printar eles ordenados
+
+//public class Ex06a{
+    //public void Executar(){
+        Random random = new Random();
+        int[] vetor = new int[10];
+        for(int i = 0; i < vetor.Length; i++){
+            vetor[i] = random.Next(1000);
+            Console.Write(vetor[i] + " ");
+        }
+
+        int temp = 0;
+
+        bool houveTroca;
+
+        do{
+            for(int i = 0; i< vetor.Length - 1; i++){
+            
+                houveTroca = false;
+
+                for(int j=0; j<vetor.Length - i - 1; j++){
+                    if(vetor[j] > vetor[j+1]){
+                    temp = vetor[j];
+                    vetor[j] = vetor[j+1];
+                    vetor[j+1] = temp;
+                    houveTroca = true;
+                    }
+                }
+            }
+
+        }while (houveTroca = false);
+        
+        Console.Write("Ordenado ↓\n\n");
+        for(int i= 0; i< vetor.Length; i++){
+            Console.Write(vetor[i] + " ");
+        }
+    //}
+//}
+
