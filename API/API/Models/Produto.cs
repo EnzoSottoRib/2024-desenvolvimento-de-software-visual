35789b22deead958da273aa12f98e8433658d8c4
Auto Logon <autologon@CSED.LAB>
2024-09-04 11:29:53 -0300

    final da aula 04/09/2024

---
+++ API/API/Models/Produto.cs
@@ -0,0 +1,31 @@
+﻿namespace API.Models; //equivalente ao package do Java
+
+public class Produto
+{
+
+    //C# - Construtor da classe
+    public Produto(){
+        Id = Guid.NewGuid().ToString(); //Guid é um identificador universal
+        CriadoEm = DateTime.Now;
+    }
+
+    //C# - Atributo com get e set
+
+    public DateTime CriadoEm { get; set; }
+    public string? Id {get; set;} // o "?" é só pra tirar a marcação falando q ta vazio
+    public string? Nome { get; set;} //"nome" minúsculo quebra a regra
+    public double Preco { get; set;}
+    public int Quantidade { get; set;}
+    
+
+    //JAVA - Atributo com get e set
+    /*
+    public double preco;
+    public double getPreco(){
+        return preco;
+    }
+    public void setPreco(double preco){
+        this.preco = preco * 3;
+    }
+    */
+}
--- API/API/Program.cs
+++ API/API/Program.cs
@@ -3,7 +3,7 @@
 // - Postman 
 // - Insomnia
 
-
+using API.Models; //equivalente ao "import" de Java
 
 
 //MINIMAL APIs
@@ -12,10 +12,46 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //EndPoints - Funcionalidades dentro das APIs
-//Configurar a URL e o método/verbo
-app.MapGet("/", () => "Hello World em C#!");
+//Request - Configurar a URL e o método/verbo HTTP
+//Response - Retornar os dados (json/xml) e 
 
-app.MapGet("/segunda_funcionalidade", () => "Hello World em C#!");
+app.MapGet("/", () => "API de Produtos");
+
+List<Produto> produtos = new List<Produto>();
+produtos.Add(new Produto(){
+    Nome = "Notebook",
+    Preco = 5000,
+    Quantidade = 54
+});
+produtos.Add(new Produto(){
+    Nome = "Desktop",
+    Preco = 3500,
+    Quantidade = 66
+});
+produtos.Add(new Produto(){
+    Nome = "Monitor",
+    Preco = 350,
+    Quantidade = 74
+});
+produtos.Add(new Produto(){
+    Nome = "Caixa de som Junkboxy",
+    Preco = 630,
+    Quantidade = 27
+});
+
+//GET: /produto/listar
+app.MapGet("/produto/listar", () => {
+    return Results.Ok(produtos);
+});
+
+//GET: /produto/cadastrar
+app.MapPost("/produto/cadastrar/{nome}", (string nome) => {
+    Produto produto = new Produto();
+    produto.Nome = nome;
+    produtos.Add(produto);
+    return Results.Ok(produtos);
+});
+
 
 app.MapGet("/retornar_endereco", () => {
     dynamic endereco = new {
@@ -33,6 +69,7 @@ app.MapGet("/retornar_endereco", () => {
 // - Receber informações pelo corpo da requisição
 // - Guardar as informações em uma lista
 
+/*
 app.MapGet("/receber_informacao_url/{nome}", (string nome) => {
     return $"O nome recebido foi: {nome}";
 });
@@ -43,11 +80,27 @@ app.MapPost("/receber_informacao_corpo", ([FromBody] UserInfo endereco) => {
 });
 
 //teste teste teste
+*/
 
 app.Run();
 
+//C# - Utilização dos gets e sets
+Produto produto = new Produto();
+produto.Preco = 5;
+Console.WriteLine("Preço: " + produto.Preco);
+
+
+//Java - Utilizacao dos gets e sets
+/*
+Produto produto = new Produto();
+produto.setPreco(5);
+Console.WriteLine("Preço: " + produto.getPreco());
+*/
+
+
+/*
 public class Endereco {
         public string Rua { get; set; }
         public int Numero { get; set; }
         public string Cep { get; set; }
-}
+} */
